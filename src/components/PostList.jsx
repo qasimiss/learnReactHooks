ba9a89bd4ea0a413bffd5940833a20ae4bc6ca73
German Vengerov <qasimis@yandex.ru>
2022-10-23 23:33:20 +0400

    add lib - react transition group

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,4 +1,5 @@
 // import { useState } from "react"
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import PostItem from "./PostItem"
 
 const PostList = ({posts, title, remove}) => {
@@ -10,9 +11,18 @@ const PostList = ({posts, title, remove}) => {
     return (
         <>
             <h1 style = {{textAlign: "center", marginTop: "20px"}}> {title}</h1>
-            {posts.map( (post, index) => 
-                <PostItem remove = {remove} number = {index+1} post = {post} key = {post.id} />
-            )}
+            <TransitionGroup>
+                {posts.map( (post, index) =>
+                    <CSSTransition
+                    key = {post.id} 
+                    timeout={500}
+                    classNames="post"
+                  > 
+                    <PostItem remove = {remove} number = {index+1} post = {post} />
+                    </CSSTransition>
+                )}
+            </TransitionGroup>
+            
         </>
     )
 }
